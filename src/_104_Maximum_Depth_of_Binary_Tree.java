25200922305e507d3d569b8748a4737538cefcd7
kashyap.shruti11@gmail.com <K@shyapfamily23>
2024-04-19 21:17:15 +0530

    Leetcode 104

---
+++ src/_104_Maximum_Depth_of_Binary_Tree.java
@@ -0,0 +1,28 @@
+import javax.transaction.TransactionRequiredException;
+import java.util.Map;
+
+
+public class _104_Maximum_Depth_of_Binary_Tree {
+
+    public int maxDepth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(leftDepth,rightDepth)+1;
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+
+    }
+
+
+}
